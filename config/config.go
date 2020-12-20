2c7aef8319fda09eb406d710d92dcc3d43d0ea67
Masayuki Onishi <omccello@gmail.com>
2020-12-20 17:49:55 +0900

    ルーティング処理を専用ディレクトリに移管

---
+++ config/config.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"log"
+	"net/http"
+
+	page "github.com/masayuki-0319/gowiki/page"
+)
+
+func SetRoute() {
+	http.HandleFunc("/view/", page.MakeHandler(page.ViewHandler))
+	http.HandleFunc("/edit/", page.MakeHandler(page.EditHandler))
+	http.HandleFunc("/save/", page.MakeHandler(page.SaveHandler))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- wiki.go
+++ wiki.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	page "github.com/masayuki-0319/gowiki/page"
+	config "github.com/masayuki-0319/gowiki/config"
 )
 
 func main() {
-	http.HandleFunc("/view/", page.MakeHandler(page.ViewHandler))
-	http.HandleFunc("/edit/", page.MakeHandler(page.EditHandler))
-	http.HandleFunc("/save/", page.MakeHandler(page.SaveHandler))
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	config.SetRoute()
 }
