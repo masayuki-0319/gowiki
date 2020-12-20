8cdf531249ec63e6215ac3a325650b453f5f24bb
Masayuki Onishi <omccello@gmail.com>
2020-12-20 16:19:35 +0900

    finish Using net/http to serve wiki pages

--- wiki.go
+++ wiki.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Hi there, I love %s!", r.URL.Path[1:])
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
@@ -15,28 +18,21 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// func main() {
-// 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-// 	p1.save()
-// 	p2, _ := loadPage("TestPage")
-// 	fmt.Println(string(p2.Body))
-// }
-
-// type Page struct {
-// 	Title string
-// 	Body  []byte
-// }
+type Page struct {
+	Title string
+	Body  []byte
+}
 
 // func (p *Page) save() error {
 // 	filename := p.Title + ".txt"
 // 	return ioutil.WriteFile(filename, p.Body, 0600)
 // }
 
-// func loadPage(title string) (*Page, error) {
-// 	filename := title + ".txt"
-// 	body, err := ioutil.ReadFile(filename)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &Page{Title: title, Body: body}, nil
-// }
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
